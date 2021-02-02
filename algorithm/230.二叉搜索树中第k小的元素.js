116b29bf55903d5f94e5bbe345a89fa02da07132
zhucb <zhucb@bsfit.com.cn>
2021-02-02 12:47:32 +0800

    feat(algorithm): 230

---
+++ algorithm/230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+	let sum = 0;
+	let res = 0;
+	function find(root) {
+		if (root === null) return;
+		find(root.left);
+		sum++;
+
+		if (sum === k) {
+			res = root.val;
+			return;
+		}
+		find(root.right);
+	}
+	find(root);
+	return res;
+};
+
+// @lc code=end
