d2a201ddb3c994c3a6d2c8727be1cef8889adbc4
zhucb <zhucb@bsfit.com.cn>
2021-03-05 17:26:54 +0800

    feat(algorithm): add No.389

--- algorithm/List/TODO/62.不同路径.js
+++ algorithm/List/TODO/62.不同路径.js
@@ -9,9 +9,29 @@
  * @param {number} m
  * @param {number} n
  * @return {number}
+ * @description
  */
-var uniquePaths = function(m, n) {
-
+const uniquePaths = (m, n) => {
+    const memo = new Array(m + 1).fill(0);
+    for(let i = 0; i < memo.length; i++) {
+        memo[i] = new Array(n + 1).fill(-1);
+    }
+    return helper(m, n, 1, 1, memo);
 };
-// @lc code=end
 
+const helper = (m, n, row, col, memo) => {
+    if(row === m && col === n) return 1;
+    if(row > m || col > n) return 0;
+
+    if(memo[row][col] === -1) {
+
+        const pathsRight = helper(m, n, row, col + 1, memo);
+        const pathsDown = helper(m, n, row + 1, col, memo);
+
+        memo[row][col] = pathsRight + pathsDown;
+    }
+
+    return memo[row][col];
+};
+
+// @lc code=end
