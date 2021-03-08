cae7eb42eb0c45969d3151fd4eeb0fb8ed2b1e60
RichieChoo <richiechoo1993@gmail.com>
2021-03-08 22:37:37 +0800

    update

--- algorithm/List/TODO/240.搜索二维矩阵-ii.js
+++ algorithm/List/TODO/240.搜索二维矩阵-ii.js
@@ -10,8 +10,27 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
-    
+var searchMatrix = function (matrix, target) {
+	if (!matrix || !matrix.length) return false;
+	const h = matrix.length;
+	const w = matrix[0].length;
+	function has(startRow, endRow, startCol, endCol) {
+		if (startRow > endRow || startCol > endCol) return false;
+		const midRow = Math.floor((endRow - startRow) / 2);
+		const midCol = Math.floor((endCol - startCol) / 2);
+		if (matrix[midRow][midCol] === target) return true;
+		if (matrix[midRow][midCol] < target) {
+			return (
+				has(midRow + 1, endRow, startCol, endCol) ||
+				has(startRow, midRow, midCol + 1, endCol)
+			);
+		} else {
+			return (
+				has(startRow, midRow - 1, midCol, endCol) ||
+				has(startRow, endRow, startCol, midCol - 1)
+			);
+		}
+	}
+	return has(0, h - 1, 0, w - 1);
 };
 // @lc code=end
-
