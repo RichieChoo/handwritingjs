0b3bcad27d8cfe3105eecead640ea5efa8b17308
RichieChoo <richiechoo1993@gmail.com>
2021-03-21 17:21:49 +0800

    update

---
+++ algorithm/Alibaba/102.二叉树的层序遍历.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+	if (!root) return [];
+	let res = [];
+	let q = [];
+	q.push(root);
+	while (q.length) {
+		let size = q.length;
+		let cPath = [];
+		for (let i = 0; i < size; i++) {
+			let cur = q.shift();
+			if (cur.left) q.push(cur.left);
+			if (cur.right) q.push(cur.right);
+			cPath.push(cur.val);
+		}
+		res.push(cPath);
+	}
+	return res;
+};
+// @lc code=end
---
+++ algorithm/Alibaba/144.二叉树的前序遍历.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+	let res = [];
+	function traverse(root) {
+		if (!root) return;
+		res.push(root.val);
+		traverse(root.left);
+		traverse(root.right);
+	}
+	traverse(root);
+	return res;
+};
+
+var preorderTraversal = function (root) {
+	let res = [],
+		stack = [];
+	if (root) stack.push(root);
+	while (stack.length) {
+		let cur = stack.pop();
+		res.push(cur.val);
+		cur.right && stack.push(cur.right);
+		cur.left && stack.push(cur.left);
+	}
+	return res;
+};
+// @lc code=end
