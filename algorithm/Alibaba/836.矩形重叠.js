7aa8c5a8bffdc1b898011fb6352c1f4deb938b37
RichieChoo <richiechoo1993@gmail.com>
2021-03-21 17:34:14 +0800

    add

---
+++ algorithm/Alibaba/836.矩形重叠.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=836 lang=javascript
+ *
+ * [836] 矩形重叠
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} rec1
+ * @param {number[]} rec2
+ * @return {boolean}
+ */
+var isRectangleOverlap = function(rec1, rec2) {
+
+};
+// @lc code=end
+
