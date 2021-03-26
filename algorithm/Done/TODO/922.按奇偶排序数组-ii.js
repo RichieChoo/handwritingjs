0c80072d9b299926182ff1a3a1d0acb897b2b19d
zhucb <zhucb@bsfit.com.cn>
2021-03-26 14:53:21 +0800

    add todo

---
+++ algorithm/Done/TODO/922.按奇偶排序数组-ii.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=922 lang=javascript
+ *
+ * [922] 按奇偶排序数组 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+
+};
+// @lc code=end
+
---
+++ algorithm/Done/TODO/979.在二叉树中分配硬币.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=979 lang=javascript
+ *
+ * [979] 在二叉树中分配硬币
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var distributeCoins = function(root) {
+
+};
+// @lc code=end
+
