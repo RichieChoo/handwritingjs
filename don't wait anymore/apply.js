f3da288a14a2c474204555d5df6d602e12544afb
RichieChoo <richiechoo1993@gmail.com>
2021-03-29 22:08:46 +0800

    add apply,bind

--- don't wait anymore/apply.js
+++ don't wait anymore/apply.js
@@ -1,19 +1,18 @@
 /*
 实现 apply
 */
-Function.prototype.myApply = function () {
-   let ctx = [].shift.call(arguments);
+Function.prototype.myApply = function (ctx, args) {
 	ctx = ctx || window;
 	const fn = Symbol();
 	ctx[fn] = this;
-	const res = ctx[fn](arguments);
+	const res = ctx[fn](...args);
 	delete ctx[fn];
 	return res;
 };
 let Person = {
 	name: "Tom",
-	say(age) {
-		console.log(this.name, age);
+	say(age,sex) {
+		console.log(this.name, age,sex);
 	},
 };
 
@@ -21,4 +20,4 @@ Person1 = {
 	name: "Tom1",
 };
 
-Person.say.myApply(Person1, [18]); //我叫Tom1我今年18
+Person.say.myApply(Person1, [18,'male']); //我叫Tom1我今年18
--- don't wait anymore/bind.js
+++ don't wait anymore/bind.js
@@ -1,3 +1,21 @@
 /*
 实现 bind
+1. 创建一个新函数。当这个新函数被调用时，第一个参数将作为它运行时的 this，
+之后的一序列参数将会在传递的实参前传入作为它的参数
+2. 一个绑定函数也能使用new操作符创建对象：这种行为就像把原函数当成构造器。
+提供的 this 值被忽略，同时调用时的参数被提供给模拟函数。
 */
+
+Function.prototype.myBind = function (ctx, ...args) {
+	let self = this;
+	var F = function () {};
+	var res = function () {
+		return self.apply(
+			this instanceof F ? this : ctx,
+			args.concat(...arguments),
+		);
+	};
+	F.prototype = this.prototype;
+	res.prototype = new F();
+	return res;
+};
