69bd1c85f660599e799cff2b854d769fe104a4d5
zhucb <zhucb@bsfit.com.cn>
2021-03-29 18:51:15 +0800

    add

--- don't wait anymore/apply.js
+++ don't wait anymore/apply.js
@@ -1,3 +1,24 @@
 /*
 实现 apply
 */
+Function.prototype.myApply = function () {
+   let ctx = [].shift.call(arguments);
+	ctx = ctx || window;
+	const fn = Symbol();
+	ctx[fn] = this;
+	const res = ctx[fn](arguments);
+	delete ctx[fn];
+	return res;
+};
+let Person = {
+	name: "Tom",
+	say(age) {
+		console.log(this.name, age);
+	},
+};
+
+Person1 = {
+	name: "Tom1",
+};
+
+Person.say.myApply(Person1, [18]); //我叫Tom1我今年18
--- don't wait anymore/call.js
+++ don't wait anymore/call.js
@@ -10,3 +10,17 @@ Function.prototype.myCall = function (ctx, ...args) {
 	delete ctx[fn];
 	return res;
 };
+
+
+let Person = {
+	name: "Tom",
+	say(age) {
+		console.log(this.name, age);
+	},
+};
+
+Person1 = {
+	name: "Tom1",
+};
+
+Person.say.myCall(Person1, 18); //我叫Tom1我今年18
