b2ca2299367c1f0f236243c549de9157b68156ec
zhucb <zhucb@bsfit.com.cn>
2021-04-02 09:55:06 +0800

    up

--- algorithm/Alibaba/215.数组中的第k个最大元素.js
+++ algorithm/Alibaba/215.数组中的第k个最大元素.js
@@ -11,6 +11,37 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
+	quickSort(nums, 0, nums.length - 1,k);
 
+	return nums[k - 1];
 };
+function quickSort(nums, left, right,k) {
+	let pIndex;
+	if(k)
+	if (left < right) {
+		pIndex = partition(nums, left, right);
+		quickSort(nums, left, pIndex - 1,k);
+		quickSort(nums, pIndex + 1, right,k);
+	}
+}
+function partition(nums, left, right) {
+	let pivot = left;
+	let index = pivot + 1;
+	for (let i = index; i <= right; i++) {
+		if (nums[i] > nums[pivot]) {
+			swap(nums, i, index);
+			index++;
+		}
+	}
+	swap(nums, pivot, index - 1);
+	return index - 1;
+}
+function swap(nums, i, j) {
+	if (i === j) return;
+	[nums[i], nums[j]] = [nums[j], nums[i]];
+}
 // @lc code=end
+
+// @after-stub-for-debug-begin
+module.exports = findKthLargest;
+// @after-stub-for-debug-end
