53cb887f3f38f0e7e80f5e018a59a868842228e0
zhucb <zhucb@bsfit.com.cn>
2021-04-12 18:29:38 +0800

    up

---
+++ prepare/3-js/emit.js
@@ -0,0 +1,48 @@
+class Emit {
+	constructor() {
+		this.events = {};
+	}
+	on(key, callback) {
+		if (this.events[key]) {
+			this.events[key].push(callback);
+		} else {
+			this.events[key] = [callback];
+		}
+	}
+	off(key, callback) {
+		if (this.events[key]) {
+			if (!this.events[key].length) {
+				delete this.events[key];
+			} else {
+				let index = this.events[key].findIndex(v => v === callback);
+				console.log("off", index);
+				this.events[key] = this.events[key].filter(v => v !== callback);
+			}
+		}
+	}
+	fire(key) {
+		this.events[key] && this.events[key].forEach(v => v());
+	}
+	once(key, callback) {
+		let self = this;
+		this.on(key, () => {
+			callback();
+			self.off(key, callback);
+		});
+	}
+}
+
+let emit = new Emit();
+let a = function () {
+	console.log("testaaa");
+};
+emit.on("test", a);
+emit.fire("test");
+emit.off("test", a);
+console.log(emit.events.test.length);
+
+emit.once("once11", function () {
+	console.log("aa once11");
+});
+emit.fire("once11");
+console.log(emit.events.once11.length);
