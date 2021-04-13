a9c3ea54e4590906d4de8af45b1d365b6c9ef3e2
zhucb <zhucb@bsfit.com.cn>
2021-04-13 13:38:36 +0800

    update

--- prepare/2-algorithm/bytedance/93.复原-ip-地址.js
+++ prepare/2-algorithm/bytedance/93.复原-ip-地址.js
@@ -4,13 +4,33 @@
  * [93] 复原 IP 地址
  */
 
-// @lc code=start
+// @lc code=i
 /**
  * @param {string} s
  * @return {string[]}
  */
-var restoreIpAddresses = function(s) {
+var restoreIpAddresses = s => {
+	let res = [];
+	const backtrack = (cPath = [], i = 0) => {
+		if (cPath.length == 4 && i == s.length) {
+			res.push(cPath.join("."));
+			return;
+		}
+		if (cPath.length == 4 && i < s.length) return;
+		for (let len = 1; len <= 3; len++) {
+			if (i + len - 1 >= s.length) return; // 加上要切的长度就越界，不能切这个长度
+			if (len != 1 && s[i] == "0") return; // 不能切出'0x'、'0xx'
 
+			const str = s.substring(i, i + len); // 当前选择切出的片段
+			if (len == 3 && Number(str) > 255) return; // 不能超过255
+
+			cPath.push(str);
+			backtrack(cPath, i + len);
+			cPath.pop();
+		}
+	};
+	backtrack();
+	return res;
 };
-// @lc code=end
 
+// @lc code=end
