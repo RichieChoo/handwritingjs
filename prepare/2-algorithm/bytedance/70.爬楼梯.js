6f114a92308e0ab0e095bbeef0d97510c92bf737
zhucb <zhucb@bsfit.com.cn>
2021-04-14 09:32:32 +0800

    update

--- prepare/2-algorithm/bytedance/70.爬楼梯.js
+++ prepare/2-algorithm/bytedance/70.爬楼梯.js
@@ -9,8 +9,24 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-
+// 超时
+// var climbStairs = function (n) {
+// 	if (n === 0 || n === 1) return 1;
+// 	let map = new Map();
+// 	if (map.get(n)) {
+// 		return map.get(n);
+// 	} else {
+// 		res = climbStairs(n - 1) + climbStairs(n - 2);
+// 		map.set(n, res);
+// 		return res;
+// 	}
+// };
+var climbStairs = function (n) {
+	let pre = 1,
+		cur = 1;
+	for (let i = 2; i <= n; i++) {
+		[pre, cur] = [cur, pre + cur];
+	}
+	return cur;
 };
 // @lc code=end
-
